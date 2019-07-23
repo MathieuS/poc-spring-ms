8d6caa69dfa5eac61b3a0eca3b846501cc9d0509
msgard <msgard@intractiv.com>
2019-07-23 17:45:43 +0200

    Initial import

---
+++ src/main/java/com/msg/poc/pocspringms/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.msg.poc.pocspringms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
